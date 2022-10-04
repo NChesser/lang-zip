46f844e9dbf83c5dddb150d8d12624024f29eab1
Nick Chesser <nick.chesser@outlook.com>
2022-10-04 19:44:57 +1030

    Initial React Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h3 className="title">
+				Lang-Zip
+			</h3>
+			<p className="subtitle">
+				By Nick Chesser
+			</p>
+
+		</div>
+	);
+}
+
+export default App;
