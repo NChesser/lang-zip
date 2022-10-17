04887e0eaf45f32b62b06ae17ada89c3ef79b59c
Nick Chesser <nick.chesser@outlook.com>
2022-10-17 21:32:43 +1030

    Update Selected Item Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -25,37 +25,14 @@ import { get } from 'lodash';
 
 // Components
 const YouTubeItem = () => {
-
-	const location = useLocation();
-	console.log("🚀 ~ file: App.tsx ~ line 32 ~ YouTubeItem ~ location", location)
-
 	// Context
-	const { selectedItem, libraryItems, handleSelectedItem } = React.useContext(LibraryContext);
+	const { selectedItem } = React.useContext(LibraryContext);
 
-	React.useEffect(() => {
-		let index = 0;
-
-		libraryItems && libraryItems.forEach((item, count) => {
-			if (location?.pathname.match(item.title.toString())) {
-				index = count;
-			}
-		});
-
-		handleSelectedItem(libraryItems[index]);
-	}, []);
-
-
-	// Props
-	// const { title, description, difficulty } = props;
-	const title = "Dreaming Spanish";
-	const description = "Hello World";
-	const difficulty = "super-beginner";
 
 	return (
 		<div className="container">
 			<h2>{selectedItem?.title}</h2>
-			<h4>{difficulty}</h4>
-			<p>{description}</p>
+			<p>{selectedItem?.description}</p>
 		</div>
 	)
 };
--- src/components/layout/Card.tsx
+++ src/components/layout/Card.tsx
@@ -2,11 +2,16 @@
     About: Basic Card for displaying Items
 */
 
+import * as React from 'react';
+
 // Routing
 import { useNavigate, Routes, Route } from "react-router-dom";
 
 // Helper Modules
 import { sample, random } from 'lodash';
+
+// Context
+import { LibraryContext } from '../../context/LibraryContext';
 
 // Components
 import CardItem from "../../pages/home/CardItem";
@@ -19,15 +24,35 @@ const Card = ({ ...props }: CardProps) => {
     // Props
     const { title, type } = props;
 
+    // Context
+    const { libraryItems, handleSelectedItem } = React.useContext(LibraryContext);
+
     // Navigate
     const navigate = useNavigate();
 
     // Functions
     const difficultyLevel = random(1, 5);
 
+    const handleNavigate = async () => {
+        let index = 0;
+        libraryItems && libraryItems.forEach((item, count) => {
+            if (title.match(item.title.toString())) {
+                index = count;
+            }
+        });
+
+        // Set Selected Item
+        handleSelectedItem(libraryItems[index]);
+
+
+        // Navigate to page
+        navigate(`/${type}/${title}`)
+    }
+
+
 
     return (
-        <div className='card' onClick={() => navigate(`/${type}/${title}`)}>
+        <div className='card' onClick={handleNavigate}>
             <div className='card-image grid'>
                 <div className={"card-icon circle " + sample(colorClasses)} />
             </div>
